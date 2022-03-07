e275f553d07827d8ec99014c52b877764bd1feda
David Brockwell <dbrockwell@my.wctc.edu>
2022-03-07 13:58:11 -0600

    Create Task class

---
+++ Task.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ticketsV3 {
+    public class Task : Ticket {
+        public string projectName { get; set; }
+        public DateTime dueDate { get; set; }
+
+        public override string entry(){
+            return $"{ticketID},{summary},{status},{priority},{submitter},{assigned},{string.Join("|", peopleWatching)},{projectName:MM}/{projectName:dd}/{projectName:yyyy},{dueDate}";
+        }
+    }
+}
